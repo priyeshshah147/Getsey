0d4be4c130d2208863eb99af24d8ebefd07d8a57
Priyesh Shah <priyesh.shah147@gmail.com>
2021-07-02 18:20:00 -0400

    fixing bugs for lanch

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 
 document.addEventListener('DOMContentLoaded',()=>{
     let root = document.getElementById('root');
-    DOMReact.render(<h1>Hi</h1>, root);
+    ReactDOM.render(<h1>Hi</h1>, root);
 })
