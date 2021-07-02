bf574b80cd6a230d6462a90c8c470ab147ce6d71
Priyesh Shah <priyesh.shah147@gmail.com>
2021-07-02 16:49:03 -0400

    Webpack install

---
+++ frontend/root.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+document.addEventListener('DOMContentLoaded',()=>{
+    let root = document.getElementById('root');
+    DOMReact.render(<h1>Hi</h1>, root);
+})
