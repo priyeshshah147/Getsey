6b4b50fd7926894a1b8a18c104d5bb0291d8122d
Priyesh Shah <priyesh.shah147@gmail.com>
2021-07-06 18:22:22 -0400

    removes some buggs

---
+++ frontend/component/session/signup_form.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+
+
+  render() {
+    return (
+      <div className="signup-form-container">
+        <form onSubmit={this.handleSubmit} className="signup-form-box">
+          <br/>
+          
+          <div className="signup-form">
+            <br/>
+            <label>Username:
+              <input type="text"
+                value={this.state.username}
+                onChange={this.update('username')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            <label>Email:
+              <input type="text"
+                value={this.state.email}
+                onChange={this.update('email')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            <label>Password:
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            {this.renderErrors()}
+            <input className="signup-submit" type="submit" value={this.props.formType} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUpForm;
